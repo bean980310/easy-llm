e1f6c7b8819b23b1974e36c229823a57333a9ca7
bean980310 <bean980310@hotmail.co.kr>
2025-01-18 07:34:22 +0900

    Update presets/__init__.py

--- presets/__init__.py
+++ presets/__init__.py
@@ -1,8 +1,10 @@
+from .ai_assistant_preset import AI_ASSISTANT_PRESET
 from .minami_asuka_char_preset import MINAMI_ASUKA_PRESET
 from .makotono_aoi_char_preset import MAKOTONO_AOI_PRESET
 from .aino_koito_char_preset import AINO_KOITO_PRESET
 
 __all__=[
+    'AI_ASSISTANT_PRESET',
     'MINAMI_ASUKA_PRESET',
     'MAKOTONO_AOI_PRESET',
     'AINO_KOITO_PRESET'
