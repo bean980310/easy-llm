750382ee3b753f343a9a6a81b05acd7751858395
bean980310 <bean980310@hotmail.co.kr>
2025-01-18 17:25:51 +0900

    Add Claude 3.5 haiku and sonnet API Model

--- src/models/api_models.py
+++ src/models/api_models.py
@@ -6,6 +6,8 @@ api_models = [
     "gpt-4o",
     "claude-3-haiku-20240307",
     "claude-3-sonnet-20240229",
-    "claude-3-opus-20240229"
+    "claude-3-opus-20240229",
+    "claude-3-5-haiku-20241022",
+    "claude-3-5-sonnet-20241022"
     # 필요 시 추가
 ]
